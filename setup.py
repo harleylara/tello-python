4db9ffa327994d35d7f7ba04d630fb17f2e1b47d
Harley Lara <harleynelsonlaraalonso1998@gmail.com>
2022-02-08 15:00:45 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='tello',
+    packages=['tello'],
     version='0.0.1',
     author="Harley Lara",
     author_email="contact@harleylara.com",
     description='Tello drones python wrapper ',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
     python_requires=">=3.6",
 )
